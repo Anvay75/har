3aa92bfac4374b32329db9842e2d23ac13ffd9a4
Anvay75 <anvaz2880@gmail.com>
2024-11-13 16:59:02 +0100

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,43 @@
+import random 
+ycp = 0
+ccp = 0
+tie = 0
+for i in range (10):
+
+    print("1.rock")
+    print("2.paper")
+    print("3.scissors")
+    
+    yc = int(input("what do you choose"))
+    cc = random.randint(1,3)
+    
+    if yc == 1 and cc == 2:
+        ccp = ccp + 1
+        print("the computer won the score is :","your score",ycp,"computer score",ccp)
+        
+    elif yc == 1 and cc == 3:
+        ycp = ycp + 1
+        print("you won the score is :","your score",ycp,"computer score",ccp)
+    
+    elif yc == 2 and cc == 1:
+        ycp = ycp + 1
+        print("you won the score is :","your score",ycp,"computer score",ccp)
+
+    
+    elif yc == 2 and cc == 3:
+        ccp = ccp + 1
+        print("the computer won the score is :","your score",ycp,"computer score",ccp)
+
+    elif yc == 3 and cc == 1:
+        ccp = ccp + 1
+        print("the computer won the score is :","your score",ycp,"computer score",ccp)
+
+    elif yc == 3 and cc == 2:
+        ycp = ycp + 1
+        print("you won the score is :","your score",ycp,"computer score",ccp)
+
+    elif yc == cc:
+        tie = tie + 1
+
+    
+
